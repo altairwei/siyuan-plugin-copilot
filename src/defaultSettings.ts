2b521af16cbfd806ace29926e0b5d5906afdfcbe
Tron <achuan-2@outlook.com>
2026-02-24 01:05:09 +0800

    🎨 在 WebView 中打开链接默认为false

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -198,7 +198,7 @@ Translate the above text enclosed with <translate_input> into {outputLanguage} w
     }>,
 
     // WebApp 相关设置
-    openLinksInWebView: true, // 是否在 webview 中打开外部链接
+    openLinksInWebView: false, // 是否在 webview 中打开外部链接
 
     // 数据迁移标志
     dataTransfer: {
